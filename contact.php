fa0cb1297587ce88315eb7e6cd4c3e9afc6fc1df
Kacper Antolak <kantolak22@zs1.nowotarski.edu.pl>
2024-01-15 20:06:32 +0100

    Initial commit

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontakt</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div>
+        <img src="bg.jpg" alt="Baner" id="bg">
+    </div>
+
+    <nav>
+        <a href="home.php">Główna</a>
+        <a href="place.php">Miejscowość</a>
+        <a href="contact.php">Kontakt</a>
+    </nav>
+
+    <div>
+        <h2>Formularz kontaktowy</h2>
+        <!-- Dodaj tutaj kod formularza -->
+    </div>
+
+    <footer>
+        <?php
+        date_default_timezone_set('Europe/Warsaw');
+        echo 'Aktualna data i godzina: ' . date('Y-m-d H:i:s') . ' ';
+        ?>
+        kantolak22@zs1.nowotarski.edu.pl
+    </footer>
+</body>
+</html>
